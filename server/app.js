c0ea50ee24ab7fc939e584b17708e5d76d659fd7
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-03-11 17:51:25 +0000

    add error handling and jest supertest

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const data = require('./data')
 const app = express();
 const results = require('./routes/results')
 
-app.use(cors());
+
 
 app.get('/', (req,res) => {
     res.send("Dogoogle");
@@ -12,6 +12,26 @@ app.get('/', (req,res) => {
 
 app.use('/results', results);
 
+
+//Below will catch requests which made it past the two above and handle them.
+//Reaching this point means that no route was able to handle the request 
+app.use((req,res,next) => {
+    const error = new Error('404: Not Found');
+    error.status = 404;
+    next(error) // forward the error
+})
+
+//This will handle errors thrown from anywhere in this application
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+
+} )
 
 
 
