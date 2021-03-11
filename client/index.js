6cf33d977422c4b41c00a08192bdc3a287d0186f
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-03-11 14:46:15 +0000

    add all data onto results page

--- client/index.js
+++ client/index.js
@@ -8,63 +8,45 @@ const resultsSection = document.getElementById('results')
 
 
 
-
-
-
-
-
-
 form.addEventListener('submit', DogoogleSearch)
-
-
 
 
 function DogoogleSearch(e){
     e.preventDefault()
     getResults()
     toggle()
-
-
 }
-
 
 
 function getResults(){
     fetch('http://localhost:3000/results')
     .then(res => res.json())
     .then (data => { data.forEach(element => {
-        let div = document.createElement('div')
-        div.textContent = element.heading + element.url + element.desc
+        const div = document.createElement('div')
+        // div.textContent = element.heading + element.url + element.desc
         resultsSection.appendChild(div)
-
+        const website = document.createElement('h3')
+        website.textContent = element.url
+        div.appendChild(website)
+        const title = document.createElement('h2')
+        title.textContent = element.heading
+        div.appendChild(title)
+        const description = document.createElement('p')
+        description.textContent = element.desc
+        div.appendChild(description)
+        })
     })
-    })
-
-
-
-
-
 }
 
 
 
-
-
-
-
-function toggle(){
-   
- 
+function toggle(){ 
     if (searchPage.style.display === 'none'){
         searchPage.style.display = 'block';
-        resultsPage.style.display ='none'
-        
+        resultsPage.style.display ='none'       
     }
-
     else{
         searchPage.style.display = 'none';
         resultsPage.style.display ='block';
     }
-
-
 }
