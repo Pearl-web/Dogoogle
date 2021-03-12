180f94758478ed2427eca7e063d25cc24bf9b343
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-03-12 10:49:37 +0000

    add test suite for frontend

---
+++ client/test/index.test.js
@@ -0,0 +1,18 @@
+
+const {randomSearch} = require('../index')
+
+
+describe ('Im feeling lucky button', () => {
+
+    it('takes me to a new page', () => {
+        expect(randomSearch()).toBeDefined()
+
+
+
+    })
+
+
+
+
+
+})
